7adb20ab01c37265036f1e7ee7dde5c5f34e7caf
emperatrizm <emperatriz.668@gmail.com>
2022-10-08 18:40:50 -0500

    summarypage

--- src/portfolioContainer/Resume/Resume.js
+++ src/portfolioContainer/Resume/Resume.js
@@ -2,7 +2,7 @@ import React from 'react'
 import './Resume.css';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faDownload } from '@fortawesome/free-solid-svg-icons'
+import { faDownload, faBadgeCheck, faCircleCheck } from '@fortawesome/free-solid-svg-icons'
 export default function Resume() {
   return (
     <section className='my-resume'>
@@ -128,7 +128,7 @@ export default function Resume() {
                   <span>80%</span>
                 </div>
                   <div className='progress'>
-                    <div className='progress-bar'></div>
+                      <div className='progress-bar'></div>
                       <div className='progress-bar'></div>
                       <div className='progress-bar'></div>
                       <div className='progress-bar'></div>
@@ -172,31 +172,29 @@ export default function Resume() {
             <div>
               <div className='skill-list'>
                 <div className='single-skill'>
-                  <div className='main-of-the-circle'>
+                  <div className='main-of-the-circle-1'>
                     <div className='circle-1'>90%</div>
                     <div className='text-circle'>Web development</div>
                   </div>
-                  <div className='main-of-the-circle'>
+                  {/* <div className='main-of-the-circle-1'>
                     <div className='circle-2'>90%</div>
                     <div className='text-circle'>Web development</div>
                   </div>
-                  <div className='main-of-the-circle'>
+                 <div className='main-of-the-circle-1'>
                     <div className='circle-3'>90%</div>
                     <div className='text-circle'>Web development</div>
                   </div>
-                  <div className='main-of-the-circle'>
+                  <div className='main-of-the-circle-1'>
                     <div className='circle-4'>90%</div>
                     <div className='text-circle'>Web development</div>
-                  </div>
+                  </div>*/} 
                   </div>
                   </div>
                 </div>
               </div>
-
       </div>
 
 {/*BASIC KNOWLEDGE*/}
-
       <div className='col-lg-6 skill-area-circle'>
         <div className='skill-box'>
           <h4 className='title'>
@@ -205,12 +203,22 @@ export default function Resume() {
             <div>
               <div className='skill-list'>
                   <div className='single-list-ul'>
-                    <li>
+                    <li className='list-check'><FontAwesomeIcon className='icons-e' icon={faCircleCheck}/>
                         Search engine marketing
                     </li>
-                    <li>
-                        <p>kjhjk</p>
+                    <li className='list-check'><FontAwesomeIcon className='icons-e' icon={faCircleCheck}/>
+                        Search engine marketing
                     </li>
+                    <li className='list-check'><FontAwesomeIcon className='icons-e' icon={faCircleCheck}/>
+                        Search engine marketing
+                    </li>
+                    <li className='list-check'><FontAwesomeIcon className='icons-e' icon={faCircleCheck}/>
+                        Search engine marketing
+                    </li>
+                    <li className='list-check'><FontAwesomeIcon className='icons-e' icon={faCircleCheck}/>
+                        Search engine marketing
+                    </li>
+                    
                   </div>
               </div>
             </div>
@@ -218,62 +226,40 @@ export default function Resume() {
       </div>
           </div>
           </div>
-          </div>
-          
-            
+          </div>     
 
     {/*EDUCATION*/}
-<div className='container'>
-      <div className='row'>
-        <div className='col-lg-6'>
-          <div className='edu-box'>
-            <h2 className='title'>
-                EDUCATION
-            </h2>
-              <div className='row'>
-                <div className='col-12'>
-                  <div className='education-list'>
-                    <div className='single-education'>
-                      <h4 className='collage-name'>ggf</h4>
-                        <p className='degree'>ughjgjh</p>
-                      <div className='description'>
-                        <p>gfghf</p>
-                      </div>
-        </div>
-
-        <div className='single-education'>
-        <h4 className='collage-name'>ggf</h4>
-        <p className='degree'>ughjgjh</p>
-        <div className='description'>
-          <p>gfghf</p>
-        </div>
-        </div>
-
-        <div className='single-education'>
-        <h4 className='collage-name'>ggf</h4>
-        <p className='degree'>ughjgjh</p>
-        <div className='description'>
-          <p>gfghf</p>
-        </div>
-        </div>
-        </div>
-
-        </div>
+    <div className='container'>  
+    <div className='row'>
+    <div className='collage-name col-6'>
+          <h2 className='title-2'>EDUCATION</h2>
+          <div className='container'>
+          <div className='row col-0.1'>
+          <div className='line-design'>
+          <span className='design-ov1'></span>
+          <span className='design-ov2'></span>
           </div>
 
+          <ul className='row col-9.9'>
+            <li className='name-list'>UNIVERSITY</li>
+            <li className='name-list'>BSC IN CSS2019 - Present</li>
+            <li className='name-list'>The University of British Columbia is a public research university with campuses in Vancouver and Kelowna, British Columbia.</li>
+          </ul>
           </div>
-        </div>
+          </div>
+          
 
-{/*EXPERIENCE*/}
-    <div className='col-lg-6'>
-    <h2 className='title'>
-        EXPERINCE
-          </h2>
     </div>
-          </div>
-          </div>
-          </div>
-          </div>  
+
+
+     {/*EXPERIENCE*/}
+    <div className='collage-name col-6'>
+          <h2 className='title-2'>Experience</h2>
+    </div>
+    </div>
+</div>
+    </div>
+    </div>
     </section>
   )
 }
