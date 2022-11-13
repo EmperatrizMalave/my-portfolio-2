200e20f059d1402ad4c445fc42da440350d57d06
emperatrizm <emperatriz.668@gmail.com>
2022-11-13 12:57:59 -0600

    various-changes

--- src/portfolioContainer/About/About.js
+++ src/portfolioContainer/About/About.js
@@ -2,6 +2,64 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>About</div>
+    <div className='content'>
+      <div className='row'>
+        <div className='col-lg-6'>
+          <div className='about-contentp'>
+              <ul className='info-list'>
+                <li>
+                  <span>First Name : </span>
+                  <span>Coromoto Emperatriz</span>
+                </li>
+                <li>
+                  <span>Last Name : </span>
+                  <span>Malave Rengel</span>
+                </li>
+                <li>
+                  <span>Age : </span>
+                  <span>29</span>
+                </li>
+                <li>
+                  <span>Phone : </span>
+                  <span>33 17621177</span>
+                </li>
+                <li>
+                  <span>Mail : </span>
+                  <span>emperatriz.668@gmail.com</span>
+                </li>
+                <li>
+                  <span>Birthdate : </span>
+                  <span>25 may 1993</span>
+                </li>
+                <li>
+                  <span>Languages native : </span>
+                  <span>Spanish</span>
+                </li>
+                <li>
+                  <span>Languages : </span>
+                  <span>English -A2 </span>
+                </li>
+                <li>
+                  <span>Address : </span>
+                  <span>Guadalajara, México </span>
+                </li>
+                <li>
+                  <span>Linkedin : </span>
+                  <span>www.linkedin.com/in/coromoto-malave-rengel</span>
+                </li>
+
+              </ul>
+          </div>
+
+        </div>
+
+
+
+        <div className='col-lg-6'>
+
+        </div>
+      </div>
+    </div>
+
   )
 }
